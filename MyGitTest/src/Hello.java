63f9e95b635994cfdfcab2d8b05d1a92b880b67a
zhaoronghui <zhaoronghui@189.cn>
2018-03-21 18:20:44 +0800

    Hello

---
+++ MyGitTest/src/Hello.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author zhaoronghui
+ *
+ */
+public class Hello {
+
+	/**
+	 * 
+	 */
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
